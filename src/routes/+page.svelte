be59f1468dbcf078321f29e65a284a2956fc9058
gomerune <minecillera@gmail.com>
2025-02-12 11:24:29 +0500

    test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <a href="./">home</a>
+    <a href="/about">about</a>
+</nav>
+
+<div >
+    <h1>Я основа</h1>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <a href="./">home</a>
+    <a href="/about">about</a>
+</nav>
+
+<div>
+    <h1>Я не основа</h1>
+</div>
